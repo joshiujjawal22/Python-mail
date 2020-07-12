c555e81ff7e4cbb7656a691a11ad280bfb1b4126
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-07-12 23:24:11 +0530

    Labelled

--- Mail.py
+++ Mail.py
@@ -1,17 +1,17 @@
-import smtplib
+import smtplib	# Library used to send mail to any Internet machine with an SMTP
 
-FROMADDR = "SenderMail"
-LOGIN    = FROMADDR
-PASSWORD = "pass"
-TOADDRS  = ["receivermail"]
+FROMADDR = "SenderMail" #Sender Mail
+LOGIN    = FROMADDR 
+PASSWORD = "pass"	# Write password of your mail
+TOADDRS  = ["receivermail"]	# Write receiver mail in list
 SUBJECT  = "Test"
 
-msg = "Whatever i wrote should be send"
+msg = "Whatever i wrote should be send" # Message you want to send
 
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.set_debuglevel(1)
-server.ehlo()
-server.starttls()
-server.login(LOGIN, PASSWORD)
+server.ehlo()	# Identify yourself to an ESMTP server using EHLO
+server.starttls()	# To upgarde insecure connection to sceure connection
+server.login(LOGIN, PASSWORD)	# To login 
 server.sendmail(FROMADDR, TOADDRS, msg)
 server.quit()
