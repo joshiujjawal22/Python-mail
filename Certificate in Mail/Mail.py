0d75f049b61f0153ce910eeefdbd8df2845cd54a
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-07-15 23:35:56 +0530

    Certificate using mail

---
+++ Certificate in Mail/Mail.py
@@ -0,0 +1,22 @@
+import smtplib	# Library used to send mail to any Internet machine with an SMTP
+from PIL import Image, ImageDraw, ImageFont  # Library used for image processing
+
+FROMADDR = "SenderMail" #Sender Mail
+LOGIN    = FROMADDR 
+PASSWORD = "pass"	# Write password of your mail
+TOADDRS  = ["receivermail"]	# Write receiver mail in list
+SUBJECT  = "Test"
+# List of names to be used in certificates
+NAMES = ['Ankur', 'Akshit' ] 
+# Font for name
+FONT = "C:/Windows/Fonts/AdobeArabic-Bold.otf"
+
+msg = "Whatever i wrote should be send" # Message you want to send
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.set_debuglevel(1)
+server.ehlo()	# Identify yourself to an ESMTP server using EHLO
+server.starttls()	# To upgarde insecure connection to sceure connection
+server.login(LOGIN, PASSWORD)	# To login 
+server.sendmail(FROMADDR, TOADDRS, msg)
+server.quit()
